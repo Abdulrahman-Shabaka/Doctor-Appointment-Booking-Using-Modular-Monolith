11afdc93e628e98c9b567e8f9fba43e26d662cce
Abdo Shabaka <Abdulrahmanshabaka98@outlook.com>
2025-01-11 21:20:26 +0200

    Define EventDispatcher in Kernel/Shared

---
+++ DoctorAppointmentBooking/Kernel/Events/EventDispatcher.cs
@@ -0,0 +1,27 @@
+﻿namespace SharedKernel.Events;
+
+public class EventDispatcher
+{
+    private readonly Dictionary<Type, List<Action<IEvent>>> _handlers = new();
+
+    public void Register<TEvent>(Action<TEvent> handler) where TEvent : IEvent
+    {
+        var eventType = typeof(TEvent);
+        if (!_handlers.ContainsKey(eventType))
+        {
+            _handlers[eventType] = [];
+        }
+
+        _handlers[eventType].Add(e => handler((TEvent)e));
+    }
+
+    public void Dispatch(IEvent @event)
+    {
+        var eventType = @event.GetType();
+        if (!_handlers.TryGetValue(eventType, out var handler1)) return;
+        foreach (var handler in handler1)
+        {
+            handler(@event);
+        }
+    }
+}
