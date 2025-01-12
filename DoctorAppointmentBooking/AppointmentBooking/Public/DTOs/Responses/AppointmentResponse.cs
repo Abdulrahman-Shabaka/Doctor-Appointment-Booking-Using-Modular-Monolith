0af661977110293b49e42512405f3b4ef6dd5532
Abdo Shabaka <Abdulrahmanshabaka98@outlook.com>
2025-01-12 11:48:28 +0200

    Add ApointmentState to Appointment model

--- DoctorAppointmentBooking/AppointmentBooking/Public/DTOs/Responses/AppointmentResponse.cs
+++ DoctorAppointmentBooking/AppointmentBooking/Public/DTOs/Responses/AppointmentResponse.cs
@@ -6,5 +6,6 @@ public class AppointmentResponse
     public Guid SlotId { get; set; }
     public Guid PatientId { get; set; }
     public string PatientName { get; set; }
+    public int State { get; set; }
     public DateTime ReservedAt { get; set; }
 }
